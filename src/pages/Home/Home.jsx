c22b718e2e2fc7a94965ba4f4b611c4feb2da162
Adem <115917288+AdamBhs@users.noreply.github.com>
2024-06-27 21:40:52 +0100

    test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
   }, [accessToken]);
 
   useEffect(() => {
-    const targetCommit = totalCommits;
+    const targetCommit = 72;
     let currentCommit = 0;
 
     const interval = setInterval(() => {
